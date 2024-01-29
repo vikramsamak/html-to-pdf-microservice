fa863c805653b337f3549890f8fee6f73fa094a3
vikramsamak <vikramsamak02@gmail.com>
2024-01-29 19:08:51 +0530

    Replacing Google sheet logger with Morgan. Fixes #7

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const { convertLogic } = require("./convertLogic");
 const app = express();
 const cors = require('cors')
-const loggerMiddleWare = require('./middlewares/loggerMiddleware.js')
+const morgan = require('morgan')
 const PORT = process.env.PORT || 2000;
 
 // MiddleWares
 app.use(cors())
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(loggerMiddleWare)
+app.use(morgan("combined"))
 
 // Routes
 app.get("/", (req, res) => {
