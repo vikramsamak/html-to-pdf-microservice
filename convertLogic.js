e0cae9a387127db2834d9188f32b93c82e561c1b
vikramsamak <vikramsamak02@gmail.com>
2024-01-29 19:41:52 +0530

    Code restructuring. Fixes #8

--- convertLogic.js
+++ convertLogic.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const convertLogic = async (htmlContent, res) => {
+const convertLogic = async (htmlContent) => {
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
@@ -16,23 +16,21 @@ const convertLogic = async (htmlContent, res) => {
   });
   try {
     const browser = await puppeteer.launch({ headless: 'new' });
+
     const page = await browser.newPage();
 
     await page.setContent(htmlContent, { waitUntil: 'domcontentloaded' });
+
     const pdfBuffer = await page.pdf({ format: 'A4' });
 
     const pdfBase64 = pdfBuffer.toString('base64');
 
-    const pdfData = {
-      dataUri: `data:application/pdf;base64,${pdfBase64}`
-    }
+    const dataUri = `data:application/pdf;base64,${pdfBase64}`
 
-    res.contentType('application/pdf')
-    res.send(pdfData.dataUri);
+    return dataUri;
   }
   catch (e) {
     console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
   }
   finally {
     await browser.close();
--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const { convertLogic } = require("./convertLogic");
 const app = express();
-const cors = require('cors')
-const morgan = require('morgan')
+const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 2000;
 
 // MiddleWares
-app.use(cors())
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(morgan("combined"))
+app.use(morgan("combined"));
 
 // Routes
 app.get("/", (req, res) => {
   res.send(`HTML-TO-PDF Service Runnig On Port ${PORT}!`);
 });
 
-app.post("/convert", (req, res) => {
+app.post("/convert", async (req, res) => {
   const { htmlContent } = req.body;
-  convertLogic(htmlContent, res);
+  try {
+    const base64encodedPdf = await convertLogic(htmlContent);
+    res.contentType('application/pdf');
+    res.send(base64encodedPdf).status(200);
+  }
+  catch {
+    res.send("Something Went Wrong!").status(500);
+  }
 });
 
 
